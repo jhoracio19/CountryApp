93f513670b72b8f081dc56c973d7ca795df98e1e
Jose Horacio <jhoracio19@hotmail.com>
2025-10-24 23:33:51 -0600

    agregando menu y footer

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,20 +1,28 @@
 import { Routes } from "@angular/router";
+import { CountryLayout } from "./layouts/CountryLayout/CountryLayout";
 import { ByCapitalPage } from "./pages/by-capital-page/by-capital-page";
 
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPage,
+    component: CountryLayout,
+    children:[
+      {
+        path: 'by-capital',
+        component: ByCapitalPage
+      },
+      {
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
   },
   // {
   //   path: 'country',
 
   // },
-  // {
-    // path: '**',
-    // redirectTo: ''
-  // }
+
 ];
 
 
